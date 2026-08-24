d84b4df6a5c6b1575b1e1c20b2fbcfa375eff1b5
rafatnasser <rafatnasser@gmail.com>
2026-08-24 10:27:34 +0300

    Add lightweight Munasabati app build

---
+++ MunasabatiLite/app/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.rafat.munasabati"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.rafat.munasabati"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
