b9629084f2e70097d7ec225ba738a17fdc9e2772
rafatnasser <rafatnasser@gmail.com>
2026-08-24 14:42:05 +0300

    Bump Munasabati to version 3.1

--- MunasabatiLite/app/build.gradle.kts
+++ MunasabatiLite/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.rafat.munasabati"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "3.0"
+        versionCode = 6
+        versionName = "3.1"
     }
 
     compileOptions {
