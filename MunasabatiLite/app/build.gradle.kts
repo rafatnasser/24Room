80f5e6861eea8ba29ed6f4d96ad409b239e0d920
rafatnasser <rafatnasser@gmail.com>
2026-08-25 07:59:32 +0300

    Bump Munasabati notification fix to 3.1.1

--- MunasabatiLite/app/build.gradle.kts
+++ MunasabatiLite/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.rafat.munasabati"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "3.1"
+        versionCode = 7
+        versionName = "3.1.1"
     }
 
     compileOptions {
