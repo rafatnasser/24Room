720157e476eec2df9a0c0c0ef1329f0e73bab5cd
rafatnasser <rafatnasser@gmail.com>
2026-08-27 08:18:44 +0300

    Bump Munasabati to v4.3

--- MunasabatiLite/app/build.gradle.kts
+++ MunasabatiLite/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.rafat.munasabati"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "4.2"
+        versionCode = 15
+        versionName = "4.3"
     }
 
     compileOptions {
